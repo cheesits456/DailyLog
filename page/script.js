342fa33b2fe4132846d109379a2ece124afa4980
Quin F. <quin@cheesits456.dev>
2025-09-25 11:26:54 -0700

    Move functions to script.js during development
    Need to be in that file to be able to have the autocomplete features for variables and class names and stuff

--- page/script.js
+++ page/script.js
@@ -12,3 +12,56 @@ const entryDirectory = path.join(os.homedir(), "Documents", "DailyLog");
 if (!fs.existsSync(entryDirectory)) fs.mkdir(entryDirectory, null, () => { });
 
 showHome();
+
+
+
+
+function showHome() {
+	page = "home";
+	document.getElementById("nav-button-new").classList.remove("active");
+
+	document.getElementById("nav-button-home").classList.add("active");
+
+	let entries = [];
+	for (const year of fs.readdirSync(entryDirectory)) {
+		for (const month of fs.readdirSync(path.join(entryDirectory, year).replace(/\\/g, "\\\\"))) {
+			for (const day of fs.readdirSync(path.join(entryDirectory, year, month).replace(/\\/g, "\\\\"))) {
+				for (const entry of fs.readdirSync(path.join(entryDirectory, year, month, day).replace(/\\/g, "\\\\"))) {
+					entries.push(JSON.parse(fs.readFileSync(path.join(entryDirectory, year, month, day, entry).replace(/\\/g, "\\\\"))));
+				};
+			};
+		};
+	};
+
+	console.log(entries);
+	
+
+	let mainHtml = `
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+	`;
+
+	mainHtml += `<h1>Test</h1>`;
+
+
+	mainHtml += `
+				</div>
+			</div>
+		</div>
+	`;
+
+	document.getElementById("main").innerHTML = mainHtml;
+
+};
+
+
+
+function showNew() {
+	page = "new";
+	document.getElementById("nav-button-home").classList.remove("active");
+	
+	document.getElementById("nav-button-new").classList.add("active");
+
+	document.getElementById("main").innerHTML = "";
+};
