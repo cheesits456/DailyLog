be3c2cd118c4f1b9c3c174916454f2574e427b15
Quin F. <quin@cheesits456.dev>
2025-09-25 10:12:55 -0700

    Create script.js

---
+++ page/script.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const os = require("os");
+
+let page = "";
+
+process.chdir(__dirname);
+
+// Set variable for diary entry directory and create the folder if it doesn't exist
+const entryDirectory = path.join(os.homedir(), "Documents", "DailyLog");
+if (!fs.existsSync(entryDirectory)) fs.mkdir(entryDirectory, null, () => { });
