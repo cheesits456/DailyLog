9489f1cc00882d576e01db7f1f285036d1aa7b81
Quin F. <quin@cheesits456.dev>
2025-09-25 10:14:28 -0700

    Create showMain() function

--- page/script.js
+++ page/script.js
@@ -10,3 +10,5 @@ process.chdir(__dirname);
 // Set variable for diary entry directory and create the folder if it doesn't exist
 const entryDirectory = path.join(os.homedir(), "Documents", "DailyLog");
 if (!fs.existsSync(entryDirectory)) fs.mkdir(entryDirectory, null, () => { });
+
+showMain();
