f39886a299d893e6492294576a9e4342ca002768
Quin F. <quin@cheesits456.dev>
2025-09-28 18:30:56 -0700

    Add page for viewing all entries

--- page/script.js
+++ page/script.js
@@ -16,8 +16,57 @@ showHome();
 
 
 
-function showEntry(date) {
+function showAll() {
+	page = "all";
 	document.getElementById("nav-button-home").classList.remove("active");
+	document.getElementById("nav-button-new").classList.remove("active");
+
+	document.getElementById("nav-button-all").classList.add("active");
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "instant",
+	});
+
+	let mainHtml = "";
+
+	for (const year of fs.readdirSync(entryDirectory)) {
+		for (const month of fs.readdirSync(path.join(entryDirectory, year).replace(/\\/g, "\\\\"))) {
+			for (const day of fs.readdirSync(path.join(entryDirectory, year, month).replace(/\\/g, "\\\\"))) {
+				for (const entry of fs.readdirSync(path.join(entryDirectory, year, month, day).replace(/\\/g, "\\\\"))) {
+					let entryData = JSON.parse(fs.readFileSync(path.join(entryDirectory, year, month, day, entry).replace(/\\/g, "\\\\")));
+					mainHtml += `
+						<div class="container margin-top">
+							<div class="row">
+								<div class="col-md-12">
+									<h1>${entryData.title}</h1>
+									<h6>${entryData.date}</h6>
+								</div>
+							</div>
+							<br>
+							<div class="row">
+								<div class="col-md-12">
+									<p>${entryData.content}</p>
+								</div>
+							</div>
+						</div>
+						<br><br>
+					`;
+				};
+			};
+		};
+	};
+
+	document.getElementById("main").innerHTML = mainHtml;
+};
+
+
+
+function showEntry(date) {
+	page = "entry";
+	document.getElementById("nav-button-all").classList.remove("active");
+	document.getElementById("nav-button-home").classList.remove("active");
+	document.getElementById("nav-button-new").classList.remove("active");
 	window.scrollTo({
 		top: 0,
 		left: 0,
@@ -31,20 +80,20 @@ function showEntry(date) {
 	const entryData = JSON.parse(fs.readFileSync(path.join(entryDirectory, year, month, day, entry)));
 
 	const mainHtml = `
-	<div class="container margin-top">
-	<div class="row">
-		<div class="col-md-12">
-			<h1>${entryData.title}</h1>
-			<h6>${entryData.date}</h6>
+		<div class="container margin-top">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>${entryData.title}</h1>
+					<h6>${entryData.date}</h6>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-md-12">
+					<p>${entryData.content}</p>
+				</div>
+			</div>
 		</div>
-	</div>
-	<br>
-	<div class="row">
-		<div class="col-md-12">
-			<p>${entryData.content}</p>
-		</div>
-	</div>
-</div>
 	`;
 
 	document.getElementById("main").innerHTML = mainHtml;
@@ -54,6 +103,7 @@ function showEntry(date) {
 
 function showHome() {
 	page = "home";
+	document.getElementById("nav-button-all").classList.remove("active");
 	document.getElementById("nav-button-new").classList.remove("active");
 
 	document.getElementById("nav-button-home").classList.add("active");
@@ -113,6 +163,7 @@ function showHome() {
 
 function showNew() {
 	page = "new";
+	document.getElementById("nav-button-all").classList.remove("active");
 	document.getElementById("nav-button-home").classList.remove("active");
 
 	document.getElementById("nav-button-new").classList.add("active");
